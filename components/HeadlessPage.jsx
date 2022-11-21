f62f08e576aeed42fdfb17aa8c18719d36f5788d
Lucian Felix Ghita <lucianfelix@gmail.com>
2022-11-21 20:35:02 +0100

    no animation

--- components/HeadlessPage.jsx
+++ components/HeadlessPage.jsx
@@ -1,116 +1,17 @@
-'use client';
+'use server';
 
 // import Head from "next/head";
-import { useState, useEffect, useContext } from 'react';
-import { WindowSizeProvider } from './ResizeProvider';
-import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import MobileHeader from './MobileHeader';
 import Panel from './Panel';
 
 export default function HeadlessPage({
+  viewType, // 'mobile' or 'desktop'
+  data,
   desktopData,
   mobileData,
   isAuthorVersion,
   host,
 }) {
-  const [viewType, setViewType] = useState('mobile');
-  const [data, setData] = useState(
-    viewType === 'mobile' ? mobileData : desktopData,
-  );
-  const [hash, setHash] = useState(null);
-  const [loadRest, setLoadRest] = useState(true);
-  const [ignoreHash, setIgnoreHash] = useState(false);
-  const [debugAnim, setDebugAnim] = useState(null);
-  const [forceView, setForceView] = useState(null);
-
-  const handleHashUpdateEvent = (e) => {
-    if (e.origin !== window.location.host && e.data.type !== 'hashUpdate') {
-      return;
-    }
-    setHash(e.data.hash);
-  };
-
-  const handleEndOfIntroAnimation = () => {
-    setLoadRest(true);
-  };
-
-  useEffect(() => {
-    window.addEventListener('message', handleHashUpdateEvent);
-    const searchParams = new URLSearchParams(window.location.search);
-
-    const debugAnimQuery = searchParams.get('debugAnim');
-    if (debugAnimQuery) {
-      setDebugAnim(debugAnimQuery);
-    }
-    const forceViewQuery = searchParams.get('forceView');
-    if (forceViewQuery) {
-      setForceView(forceViewQuery);
-    }
-
-    return () => window.removeEventListener('message', handleHashUpdateEvent);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // reset content on width change
-  const windowSize = useContext(WindowSizeProvider);
-  useEffect(() => {
-    if (windowSize.width === null || forceView) {
-      return;
-    }
-    // reset data on resize if passing breakpoint with another viewType set
-    if (windowSize.width > 840 && viewType !== 'desktop') {
-      setData(null);
-      setIgnoreHash(false);
-    }
-    if (windowSize.width <= 840 && viewType !== 'mobile') {
-      setData(null);
-      setIgnoreHash(false);
-    }
-  }, [windowSize.width, forceView, viewType]);
-
-  // refresh scrolltrigger on height change
-  useEffect(() => {
-    if (windowSize.height === null) {
-      return;
-    }
-    ScrollTrigger.refresh();
-  }, [windowSize.height]);
-
-  // setData depending on width if data is null
-  // data must be set to null for one render, in order to fully remove animations
-  useEffect(() => {
-    if (data !== null) {
-      return;
-    }
-    if (forceView) {
-      if (forceView.toLocaleLowerCase() === 'desktop') {
-        setData(desktopData);
-        setViewType('desktop');
-      }
-      if (forceView.toLocaleLowerCase() === 'mobile') {
-        setData(mobileData);
-        setViewType('mobile');
-      }
-    } else {
-      if (windowSize.width > 840 || windowSize.width === null) {
-        setData(desktopData);
-        setViewType('desktop');
-      } else {
-        setData(mobileData);
-        setViewType('mobile');
-      }
-    }
-    ScrollTrigger.refresh();
-  }, [data, desktopData, mobileData, windowSize.width, forceView]);
-
-  // if a hash exists don't wait for first animation to finish
-  useEffect(() => {
-    if (window.location.hash) {
-      setLoadRest(true);
-      setHash(window.location.hash);
-    }
-  }, [viewType]);
-
   console.log('HeadlessPage for ' + viewType + ' rendered at ', new Date());
 
   return (
@@ -144,12 +45,11 @@ export default function HeadlessPage({
                 panelNr={index}
                 settings={{ viewType }}
                 key={index}
-                runOnEnd={index === 0 ? handleEndOfIntroAnimation : null}
+                runOnEnd={null}
                 isAuthorVersion={isAuthorVersion}
                 host={host}
                 hash={hash}
                 ignoreHash={ignoreHash}
-                setIgnoreHash={setIgnoreHash}
               />
             );
           })}
--- components/Panel.jsx
+++ components/Panel.jsx
@@ -1,20 +1,13 @@
-'use client';
+'use server';
 
 //import dynamic from 'next/dynamic';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import LayerImage from '../components/LayerImage';
 import TextLayer from '../components/TextLayer';
 import PointTextMap from './PointTextMap';
-import { TimelineProvider } from './TimelineWrapper';
 import { scrollToId } from '../components/utils';
 import Header from './Header';
 import Background from './Background';
-
-// const Background = dynamic(() => import('../components/Background'));
-// const Header = dynamic(() => import('../components/Header'));
-// const LayerImage = dynamic(() => import('./LayerImage'));
-// const TextLayer = dynamic(() => import('./TextLayer'));
-// const PointTextMap = dynamic(() => import('./PointTextMap'));
 
 export default function Panel({
   panel,
@@ -25,10 +18,7 @@ export default function Panel({
   host,
   hash,
   ignoreHash,
-  setIgnoreHash,
 }) {
-  const createTimeline = useContext(TimelineProvider);
-
   const lookupObject = {
     image: LayerImage,
     'Image Layer': LayerImage,
@@ -36,32 +26,6 @@ export default function Panel({
     'Text Layer': TextLayer,
     'Shoppable Moment Layer': PointTextMap,
   };
-
-  useEffect(() => {
-    if (!createTimeline) {
-      return;
-    }
-    createTimeline(
-      panel?.animations?.timelineAnimations,
-      panel?.animations?.timelineAnimationSettings,
-      runOnEnd,
-    );
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    createTimeline,
-    panel?.animations?.timelineAnimationSettings,
-    panel?.animations?.timelineAnimations,
-  ]); // adding runOnEnd makes into animations re-run on end
-
-  useEffect(() => {
-    // if hash matches id, scroll this panel
-    if (hash === '#' + panel.id && !ignoreHash) {
-      scrollToId(hash);
-      // stops page from scrolling to hash every time viewport is resized
-      setIgnoreHash(true);
-    }
-  }, [hash, ignoreHash, panel.id, setIgnoreHash]);
 
   return (
     <div className={`panel ${panel?.dark ? 'darkPanel' : ''} `} id={panel.id}>
