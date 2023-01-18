c1a0f5b3ec531c6e14c5fc7a62d04d74a4a6b0ac
Ghita Lucian <ghita@adobe.com>
2023-01-18 14:54:47 +0100

    next/image for adventures

--- components/AdventureCard.jsx
+++ components/AdventureCard.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Adobe. All rights reserved.
+ * Copyright 2023 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -12,14 +12,21 @@
  */
 
 import Link from 'next/link'
+import Image from 'next/image';
 
 export default function AdventureCard({ slug, title, price, duration, imageSrc }) {
+  const aboveFold = ['Bali Surf Camp'].includes(title);
   return (
     <div key={slug} className="group relative">
       <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-        <img
+        <Image
           src={imageSrc}
           alt={title}
+          width={542}
+          height={605}
+          priority={aboveFold}
+          loading={aboveFold ? 'eager' : 'lazy'}
+          sizes="50vw"
           className="w-full h-full object-center object-cover lg:w-full lg:h-full"
         />
       </div>
