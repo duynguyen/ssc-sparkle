497ee04854596ce3bd95f4e08e0fa9ee95ee9a17
Duy Nguyen <dunguyen@adobe.com>
2023-03-16 16:57:34 +0100

    only load universal editor script on author

--- app/adventures/page.jsx
+++ app/adventures/page.jsx
@@ -26,7 +26,7 @@ export default async function Page() {
   const title = await pageClient.getItem(itemPath);
 
   return (<section className="">
-    <script src="universal-editor-embedded.js" async></script>
+    {NEXT_PUBLIC_AEM_HOST.includes('author') && <script src="universal-editor-embedded.js" async></script>}
     <div className="bg-white">
       <div className="max-w-2xl px-4 py-10 mx-auto sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
